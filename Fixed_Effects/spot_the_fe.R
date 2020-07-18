de82ec3b8c9bcffa421c39ad06e1de90b115029a
NickCH-K <nhuntington-klein@fullerton.edu>
2020-07-18 14:56:22 -0700

    Added DID, FE, added a hypothesis test to polynomial

---
+++ Fixed_Effects/spot_the_fe.R
@@ -0,0 +1,40 @@
+if (!('patchwork' %in% installed.packages())) {
+  install.packages('patchwork')
+}
+library(patchwork)
+
+pnlsmall <- mathpnl %>%
+  filter(distid %in% c(50220,2010,27020,68030))
+
+pnlsmall <- pnlsmall %>%
+  mutate(distid = factor(distid)) %>%
+  mutate(fepred = predict(lm(math4~expp + distid, data = pnlsmall)),
+         polypred = predict(lm(math4~expp + I(expp^2), data = pnlsmall)))
+
+a <- ggplot(pnlsmall,
+            aes(x = expp, y = math4, color = distid)) +
+  geom_point(size = 2)+ 
+  geom_smooth(aes(color = NULL), method = 'lm', se = FALSE, color = 'blue', size = 1) +
+  theme_minimal() +
+  labs(title = 'Graph A')
+b <- ggplot(pnlsmall,
+            aes(x = expp, y = math4, color = distid, group = distid)) +
+  geom_point(size = 2)+ 
+  geom_smooth(method = 'lm', se = FALSE, color = 'blue', size = 1) +
+  theme_minimal() +
+  labs(title = 'Graph B')
+c <- ggplot(pnlsmall,
+            aes(x = expp, y = math4, color = distid)) +
+  geom_point(size = 2)+ 
+  geom_line(aes(y = polypred, color = NULL), color = 'blue', size = 1) +
+  theme_minimal() +
+  labs(title = 'Graph C')
+d <- ggplot(pnlsmall,
+            aes(x = expp, y = math4, color = distid, group = distid)) +
+  geom_point(size = 2)+ 
+  geom_line(aes(y = fepred), color = 'blue', size = 1) +
+  theme_minimal() +
+  labs(title = 'Graph D')
+
+e<- (a + b)/(c + d)
+print(e)
